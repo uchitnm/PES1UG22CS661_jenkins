5ab83178e418f5b8025b955a92d8db41d34c5846
Uchit N M <uchitnm@Uchits-MacBook-Pro.local>
2025-03-13 15:10:15 +0530

    changes in cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "This, is lab 8 CC.\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
