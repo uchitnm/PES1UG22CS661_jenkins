e4c16ef4a900a3f312170fdfd689b2db07b0dc8f
Uchit N M <uchitnm@Uchits-MacBook-Pro.local>
2025-03-13 16:17:35 +0530

    changes in cpp and Jenkinsfile

---
+++ main/hello_task5.cpp
@@ -0,0 +1,12 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This, is lab 8 CC.\n";
+  cout << "task 5.\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
