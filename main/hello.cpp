cc554a82d96931f764e6a867ee9dc2b8a1172c11
Uchit N M <uchitnm@Uchits-MacBook-Pro.local>
2025-03-13 15:40:45 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "This, is lab 8 CC.\n";
+  cout << "Commiting for the task\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
